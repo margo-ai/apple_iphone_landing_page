85f9de009d1e4548190b372aed9042cc8ac9d745
Margo <plokhova.margarita@yandex.ru>
2022-10-19 22:08:30 +0300

    created top video section

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import HeroSection from "./sections/HeroSection";
 import Quote from "./sections/Quote";
 import { GlobalStyle } from "./styles/GlobalStyle";
 
@@ -7,6 +8,7 @@ function App() {
 	<>
 		<GlobalStyle />
 		<Quote />
+		<HeroSection />
 	</>
   );
 }
