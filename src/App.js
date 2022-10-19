fb1e405331ba1b0b03c69985872ae6a7f1ecc847
Margo <plokhova.margarita@yandex.ru>
2022-10-19 20:00:58 +0300

    created quote section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import Quote from "./sections/Quote";
 import { GlobalStyle } from "./styles/GlobalStyle";
 
 
 function App() {
   return (
-    <>
+	<>
 		<GlobalStyle />
-    </>
+		<Quote />
+	</>
   );
 }
 
